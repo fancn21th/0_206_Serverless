ecda471407c18dc3fb4f95037b7cfde63c409389
fancn21th <fancn21th@gmail.com>
2022-08-12 17:12:50 +0800

    endpoint

--- handler.js
+++ handler.js
@@ -1,6 +1,14 @@
 // module.exports.run = (event, context, callback) => {
 //   callback(null, "Hello World");
 // };
-module.exports.run = (event) => {
-  return Promise.resolve("Hello World");
+// module.exports.run = (event) => {
+//   return Promise.resolve("Hello World");
+// };
+module.exports.run = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello World",
+    }),
+  };
 };
