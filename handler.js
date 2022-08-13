9feaaf684e8acda6ed4c979ce040d4de64814597
fancn21th <fancn21th@gmail.com>
2022-08-13 09:32:12 +0800

    cron job

--- handler.js
+++ handler.js
@@ -1,14 +1,23 @@
-// module.exports.run = (event, context, callback) => {
-//   callback(null, "Hello World");
-// };
-// module.exports.run = (event) => {
-//   return Promise.resolve("Hello World");
-// };
-module.exports.run = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello World",
-    }),
+require("dotenv").config();
+console.log(process.env); // remove this after you've confirmed it working
+
+const fetch = require("node-fetch");
+
+module.exports.run = async (event, context) => {
+  const body = {
+    msgtype: "text",
+    text: {
+      content: process.env.BOT_MSG,
+    },
   };
+
+  const response = await fetch(process.env.BOT_URL, {
+    method: "post",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await response.json();
+
+  console.log(data);
 };
